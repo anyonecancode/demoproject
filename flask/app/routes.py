4adb3f841563bebf470e05b2177a2742f39ef38c
anyonecancode <pschwei1@gmail.com>
2018-11-26 22:09:59 -0500

    Began the Explore views

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -2,6 +2,21 @@ from flask import render_template
 from app import app
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', section='home')
+
+
+@app.route('/explore')
+@app.route('/explore/<category>')
+@app.route('/explore/<category>/<style>')
+def explore(category = None, style = None):
+    title = 'Explore'
+    section = 'explore'
+
+    template = 'explore.html'
+    if category:
+        template = 'explore.category.html'
+    if style:
+        template = 'explore.category.style.html'
+
+    return render_template(template, title = title, section = 'explore')
