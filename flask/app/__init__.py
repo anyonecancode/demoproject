7a43b0ca7fb174084eb09013605cd019563d4e41
anyonecancode <pschwei1@gmail.com>
2018-12-08 14:47:31 -0500

    Sorting table supported on backend

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -9,4 +9,4 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
-from app import routes, models
+from app import routes, errors, models
--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template, request, url_for
 from app import app
-from app.models import Beer
+from app.models import Beer, Category, Brewery
 
 @app.route('/')
 def index():
@@ -11,20 +11,61 @@ def index():
 @app.route('/explore/<category>')
 @app.route('/explore/<category>/<style>')
 def explore(category = None, style = None):
+    page = request.args.get('page', 1, type=int)
     title = 'Explore'
     section = 'explore'
     template = 'searchresults.html'
 
-    beers = Beer.query.all()
+    q = Beer.query
+    orderby = Beer.name
+    orderDesc = False
+    orderbyField = None
+
+    orderbyparam = request.args.get('orderby', None, str)
+    if (orderbyparam):
+        parts = orderbyparam.split('|')
+        orderbyField = parts[0]
+        if len(parts) == 2 and parts[1].lower() == 'desc':
+            orderDesc = True
+
+    if orderbyField:
+        if orderbyField == 'category':
+            q = q.join(Category)
+            orderby = Category.name
+        if orderbyField == 'brewery':
+            q = q.join(Brewery)
+            orderby = Brewery.name
+        if orderbyField == 'region':
+            q = q.join(Brewery)
+            orderby = Brewery.state
+        if orderbyField == 'country':
+            q = q.join(Brewery)
+            orderby = Brewery.country
+
+    if orderDesc:
+        orderby = orderby.desc()
+
+    q = q.order_by(orderby)
+
+    count = q.count()
+    pagination = q.paginate(page,25)
 
     results = []
-    for b in beers:
+    for beer in pagination.items:
         results.append({
-            'beer': b.name
-            ,'category': b.category
-            ,'style': b.style
-            ,'abv': b.abv
-            ,'brewery': b.brewery
+            'beer': beer.name
+            ,'category': beer.category
+            ,'style': beer.style
+            ,'abv': beer.abv
+            ,'brewery': beer.brewery
                 })
 
-    return render_template(template, title = title, section = 'explore', results = results)
+    pagecontrols = {
+            'resultscount': count
+            ,'page': pagination.page
+            ,'pages': pagination.pages
+            ,'prev': url_for('explore', page=pagination.prev_num) if pagination.has_prev else None
+            ,'next': url_for('explore', page=pagination.next_num) if pagination.has_next else None
+            }
+    print(pagecontrols['next'])
+    return render_template(template, title = title, section = 'explore', results = results, pagecontrols = pagecontrols)
