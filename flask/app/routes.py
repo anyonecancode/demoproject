e9e798109071a8fcff0de05f07d2787a4ff44b7e
anyonecancode <pschwei1@gmail.com>
2018-12-08 23:50:40 -0500

    Refactored orderby query builder. Stubbed out filterby query builder. Tracking models to join.

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -11,18 +11,29 @@ def index():
 @app.route('/explore/<category>')
 @app.route('/explore/<category>/<style>')
 def explore(category = None, style = None):
+    #Pagination support
     page = request.args.get('page', 1, type=int)
     title = 'Explore'
     section = 'explore'
     template = 'searchresults.html'
+    #Default query. Simple SELECT FROM beers if no filter or sort, and the base we join against otherwise.
+    base_query = Beer.query
 
-    q = Beer.query
+    #We'll need to do some if we filter and/or sort, but we want to make sure we only join on a given
+    #Model once, hence the set data structure
+    joined_models = set()
+
+    #filterparams = request.args.getlist('filterby', str)
+    #f, joined_models = _buildFilterByQuery(base_query, filterparams, joined_models)
 
     orderby = request.args.get('orderby', None, str)
-    q = _buildOrderyByQuery(q, orderby)
+    base_query, joined_models = _buildOrderyByQuery(base_query, orderby, joined_models)
 
-    count = q.count()
-    pagination = q.paginate(page,25)
+    for m in joined_models:
+        base_query =  base_query.join(m)
+
+    count = base_query.count()
+    pagination = base_query.paginate(page,25, error_out=False)
 
     results = []
     for beer in pagination.items:
@@ -45,27 +56,42 @@ def explore(category = None, style = None):
     return render_template(template, title = title, section = 'explore', results = results, pagecontrols = pagecontrols)
 
 
-def _buildOrderyByQuery(baseQuery, orderbyparams):
+def _buildFilterByQuery(baseQuery, filterparams, joined_models):
+    if filterparams is None:
+        return baseQuery, joined_models
+    return baseQuery, joined_models
+
+
+
+
+def _buildOrderyByQuery(base_query, orderby_param, joined_models = set()):
     orderby = Beer.name #Default
 
-    if orderbyparams is None:
-        return baseQuery.order_by(orderby.asc())
+    if orderby_param is None:
+        return base_query.order_by(orderby.asc()), joined_models
 
-    parts = orderbyparams.split('|')
-    orderbyField = parts[0]
-    orderDesc = True if (len(parts) == 2 and parts[1].lower() == 'desc') else False
+    orderDesc = False
+    if orderby_param[0] == '-':
+        orderDesc = True
+        orderby_param = orderby_param[1:]
 
-    if orderbyField == 'category':
-        baseQuery = baseQuery.join(Category)
+
+    if orderby_param == 'category':
+        joined_models.add(Category)
         orderby = Category.name
-    if orderbyField == 'brewery':
-        baseQuery = baseQuery.join(Brewery)
+    if orderby_param == 'brewery':
+        joined_models.add(Brewery)
         orderby = Brewery.name
-    if orderbyField == 'country':
-        baseQuery = baseQuery.join(Brewery)
+    if orderby_param == 'region':
+        joined_models.add(Brewery)
+        orderby = Brewery.state
+    if orderby_param == 'country':
+        joined_models.add(Brewery)
         orderby = Brewery.country
 
     if orderDesc:
         orderby = orderby.desc()
 
-    return baseQuery.order_by(orderby)
+    return base_query.order_by(orderby), joined_models
+
+
